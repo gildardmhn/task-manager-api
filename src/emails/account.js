d8b42c4daeb08e246deb61c73fd6aed2b8711ca8
GildardMyehouenouOSF <gildard.myehouenou@osf-global.com>
2019-09-10 18:13:12 -0300

    Changed the email

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'gildardmhn93@gmail.com',
+        from: 'taskmanagerapp@taskapi.com',
         subject: 'Welcome to the Task Manager App',
         text: `Welcome to the app, ${name}. Let me know how you get along  with the app.`
     })
